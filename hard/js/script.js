8f0d3590dbe07221d70babfc1ab09fc5fb7e0c45
Kate Tsareva <katetsareva3012@mail.ru>
2020-01-27 21:55:07 +0300

    fix: hard 1-c | hard/js/script.js

--- hard/js/script.js
+++ hard/js/script.js
@@ -1,34 +1,39 @@
 let lang = prompt('Выберете язык / Choose a language (ru/en)', 'ru');
 
 let weekRu = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
-	weekEn = ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su'];
+  weekEn = ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su'];
 
 // a
 if (lang === 'ru') {
-	console.log('Дни недели: ', weekRu);
+  console.log('Дни недели: ', weekRu);
 } else if (lang === 'en') {
-	console.log('Days of week: ', weekEn);
+  console.log('Days of week: ', weekEn);
 } else {
-	console.log('Язык не найден / Language not found');
+  console.log('Язык не найден / Language not found');
 }
 
 // b 
 switch (lang) {
-	case 'ru':
-		console.log('Дни недели: ', weekRu);
-		break;
-	case 'en':
-		console.log('Days of week: ', weekEn);
-		break;
-	default:
-		console.log('Язык не найден / Language not found');
+  case 'ru':
+    console.log('Дни недели: ', weekRu);
+    break;
+  case 'en':
+    console.log('Days of week: ', weekEn);
+    break;
+  default:
+    console.log('Язык не найден / Language not found');
 }
+
 // c
-let languages = [weekRu, weekEn];
-let indexLang = (lang === 'ru') ? 0 : ((lang === 'en') ? 1 : console.log('Язык не найден / Language not found'));
-console.log('Дни недели / Days of week: ', languages[indexLang]);
+let languages = {
+  'ru' : weekRu,
+  'en' : weekEn
+}
+
+console.log(languages[lang]);
 
 
 // 2
 let namePerson = prompt('Введите имя');
-let statusPerson = (namePerson === 'Артем') ? console.log('директор') : ((namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент'));
+let statusPerson = (namePerson === 'Артем') ? console.log('директор') :
+  ((namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент'));
