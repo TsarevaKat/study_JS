37712f76d6775015a8faeec7cf2ac7031a8c1b85
Kate Tsareva <katetsareva3012@mail.ru>
2020-01-30 00:45:40 +0300

    fix: lesson04 hard | hard

--- hard/js/script.js
+++ hard/js/script.js
@@ -1,19 +1,19 @@
-let userString = prompt('Введите строку');
+const userString = prompt('Введите строку');
 
 function processingString(str) {
-  if (typeof(str) !== 'string') {
+  if (!isNaN(str)) {
     console.log('Это не строка');
-  } else {
-    // первый вариант
-    console.log(str.trim());
-    // второй вариант через регулярку
-    console.log(str.replace(/^\s*/, '').replace(/\s*$/, ''));
+    return 0;
   }
+
+  str = str.replace(/^\s*/, '').replace(/\s*$/, '');
+
   if (str.length > 30) {
-    str = str.substr(0, 29);
+    str = str.slice(0, 30);
     str += '...';
-    console.log(str);
   }
+  console.log(str);
+  return 0;
 }
 
 processingString(userString);
