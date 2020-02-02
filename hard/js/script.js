8688b2f17cff6e20b7f3db815be639f1c84dcf9e
Kate Tsareva <katetsareva3012@mail.ru>
2020-02-02 21:23:11 +0300

    feature: lesson07 hard done | hard

--- hard/js/script.js
+++ hard/js/script.js
@@ -1,1 +1,21 @@
-const week = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+const week = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+let date = new Date();
+console.log('date: ', date);
+console.log(date.getDay());
+
+for (let item of week) {
+  let div = document.createElement('div');
+  div.innerHTML = item;
+  if (item === 'вс' || item === 'сб') {
+    div.style.cssText = 'font-style: italic';
+  }
+  if (item === week[date.getDay()]) {
+    div.style.cssText = 'font-weight: 700;';
+  }
+  if ((item === 'вс' || item === 'сб') && item === week[date.getDay()]) {
+    div.style.cssText = 'font-style: italic;font-weight: 700;';
+
+  }
+  document.body.append(div);
+}
+
