fde74c8f850112660657cc458c8253469dcfd906
Kate Tsareva <katetsareva3012@mail.ru>
2020-02-27 15:15:05 +0300

    chore: lesson14-hard

--- easy-first/js/script.js
+++ easy-first/js/script.js
@@ -1,50 +1,78 @@
-'use strict';
-class DomElement {
-  constructor(selector = '', height = '', width = '', bg = '', fontSize = '', text = '') {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-    this.text = text;
-  }
+document.addEventListener('DOMContentLoaded', () => {
 
-  addElement() {
-    let block;
-    const char = this.selector.charAt(0);
-    console.log('char: ', char);
-    if (char === '.') {
-      block = document.createElement('div');
-      block.classList.add(this.selector.slice(1));
-    } else if (char === '#') {
-      block = document.createElement('p');
-      block.setAttribute('id', this.selector.slice(1));
+  'use strict';
+  class DomElement {
+    constructor(
+      selector = '',
+      position = '', top = 0, left = 0,
+      height = '', width = '',
+      bg = '',
+      fontSize = '',
+      text = '') {
+      this.selector = selector;
+      this.position = position;
+      this.top = top;
+      this.left = left;
+      this.height = height;
+      this.width = width;
+      this.bg = bg;
+      this.fontSize = fontSize;
+      this.text = text;
     }
-    block.style.cssText = `
-      width: ${this.width};
-      height: ${this.height};
-      background: ${this.bg};
-      font-size: ${this.fontSize};
-    `;
-    block.textContent = this.text;
-    document.body.appendChild(block);
+
+    addElement() {
+      let block;
+      const char = this.selector.charAt(0);
+      if (char === '.') {
+        block = document.createElement('div');
+        block.classList.add(this.selector.slice(1));
+      } else if (char === '#') {
+        block = document.createElement('p');
+        block.setAttribute('id', this.selector.slice(1));
+      }
+      block.style.cssText = `
+        position: ${this.position};
+        top: ${this.top};
+        left: ${this.left};
+        width: ${this.width};
+        height: ${this.height};
+        background: ${this.bg};
+        font-size: ${this.fontSize};
+      `;
+      block.textContent = this.text;
+      document.body.appendChild(block);
+    }
+
+
   }
-}
 
 
-const newElenent = new DomElement('#block', '10px', '100%', 'red', '10px', 'Красный'),
-  newElenent2 = new DomElement('.block', '12px', '100%', 'orange', '12px', 'Оранжевый'),
-  newElenent3 = new DomElement('.block', '14px', '100%', 'yellow', '14px', 'Желтый'),
-  newElenent4 = new DomElement('.block', '16px', '100%', 'green', '16px', 'Зеленый'),
-  newElenent5 = new DomElement('.block', '18px', '100%', 'blue', '18px', 'Голубой'),
-  newElenent6 = new DomElement('.block', '20px', '100%', 'darkblue', '20px', 'Синий'),
-  newElenent7 = new DomElement('.block', '22px', '100%', 'purple', '22px', 'Фиолетовый');
+  const newElenent = new DomElement('#block', 'absolute', 0, 0, '100px', '100px', 'red', '', '');
 
-newElenent.addElement();
-newElenent2.addElement();
-newElenent3.addElement();
-newElenent4.addElement();
-newElenent5.addElement();
-newElenent6.addElement();
-newElenent7.addElement();
+  newElenent.addElement();
 
+  const block = document.getElementById('block');
+  document.addEventListener('keydown', (e) => {
+    let keyCode = e.keyCode;
+    // вниз
+    if (keyCode === 40) {
+      let topBlock = parseInt(block.style.top) + 10;
+      block.style.top = topBlock + 'px';
+    }
+    // вверх
+    if (keyCode === 38) {
+      let topBlock = parseInt(block.style.top) - 10;
+      block.style.top = topBlock + 'px';
+    }
+    // вправо
+    if (keyCode === 39) {
+      let leftBlock = parseInt(block.style.left) + 10;
+      block.style.left = leftBlock + 'px';
+    }
+    // влево
+    if (keyCode === 37) {
+      let leftBlock = parseInt(block.style.left) - 10;
+      block.style.left = leftBlock + 'px';
+    }
+  });
+});
